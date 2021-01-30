724d89051a7126d0addc28e40a5291ae0291c95b
AlexanderDark <alexanderdark@gmail.com>
2021-01-30 18:31:08 +0300

    VtoroeDZ

--- SecondClassWork/Junit-tests/src/test/java/OtusInfo.java
+++ SecondClassWork/Junit-tests/src/test/java/OtusInfo.java
@@ -3,6 +3,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -17,23 +18,22 @@ public class OtusInfo {
     }
     @Test
     public void openUrl () {
-        driver.get("https://design.1cmaker.com/login");
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        driver.findElement(By.id("email")).sendKeys("alexanderdark@gmail.com");
-        driver.findElement(By.id("password")).sendKeys("ChUrKiVgOrI");
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        driver.findElement(By.className("MuiButton-label")).click();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        driver.findElement(By.xpath("//p[contains(text(),‘Последнее изменение’)]/../div[1]//div[2]")).click();
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        /*driver.get("https://otus.ru");
+        driver.get("https://otus.ru");
         driver.findElement(By.className("header2__auth-reg")).click();
         String cssSelector = ".js-input[placeholder = \"Электронная почта\"]";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.cssSelector(cssSelector)).sendKeys("dara@gkomega.ru");
         cssSelector = "[type = \"password\"]";
         driver.findElement(By.cssSelector(cssSelector)).sendKeys("@3@N7vr7!cpW");
         cssSelector = ".new-input-line_relative .new-button_md[type = \"submit\"]";
-        driver.findElement(By.cssSelector(cssSelector)).click();*/
+        driver.findElement(By.cssSelector(cssSelector)).click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://otus.ru/lk/biography/personal");
+        driver.get("https://otus.ru/lk/biography/personal");
+        driver.findElement(By.name("date_of_birth")).sendKeys("03.01.1989"+ Keys.ENTER);
+        /*driver.findElement(By.cssSelector("#id_ready_to_relocate_1 .radio__label :first-child")).click();*/
+        driver.findElement(By.cssSelector("[data-ajax-slave=\"/lk/biography/cv/lookup/cities/by_country/\"] [title=\"Россия\"]")).click();
+        driver.findElement(By.cssSelector("[title=\"Сохранить и заполнить позже\"]")).click();
     }
 /*    @After
     public void clearDriver () {
---
+++ VtoroeDZ/src/test/java/VtoroeDZ.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class VtoroeDZ {
+    protected static WebDriver driver;
+    @Before
+public void setUo () {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver =new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+    @Test
+    public void equalsTitle () {
+        driver.get("https://yandex.ru");
+        Assert.assertEquals("Яндекс", driver.getTitle());
+    }
+    @Test
+    public void numbers () {
+        driver.get("https://msk.tele2.ru/shop/number");
+        driver.findElement(By.id("searchNumber")).sendKeys("97"+ Keys.ENTER);
+        WebElement explicitWait = (new WebDriverWait(driver, 3))
+                .until(ExpectedConditions.presenceOfElementLocated(By.className("number-box")));
+    }
+    @After
+    public void closeDriver () {
+        if (driver != null) {
+            driver.close();
+        }
+    }
+}
